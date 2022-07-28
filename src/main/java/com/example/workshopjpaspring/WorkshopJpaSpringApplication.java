d22f131c61e9f3d47385db3831d83cad63bea221
Henrique Barucco <henrique_barucco@hotmail.com>
2022-07-28 15:44:14 -0300

    Project created

---
+++ src/main/java/com/example/workshopjpaspring/WorkshopJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopjpaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopJpaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopJpaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workshopjpaspring/WorkshopJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopjpaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJpaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
