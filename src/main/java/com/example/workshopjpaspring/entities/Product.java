717cde6683c9f13a41bfcbd94b7952deee9291fa
Henrique Barucco <henrique_barucco@hotmail.com>
2022-08-01 20:03:09 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/example/workshopjpaspring/entities/Product.java
+++ src/main/java/com/example/workshopjpaspring/entities/Product.java
@@ -1,5 +1,7 @@
 package com.example.workshopjpaspring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -22,6 +24,9 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -76,6 +81,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
