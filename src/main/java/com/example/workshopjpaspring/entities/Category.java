4bba6ca8ce592d0fe21f5e1638a4bbeb43e97249
Henrique Barucco <henrique_barucco@hotmail.com>
2022-08-01 18:53:45 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/example/workshopjpaspring/entities/Category.java
+++ src/main/java/com/example/workshopjpaspring/entities/Category.java
@@ -1,5 +1,7 @@
 package com.example.workshopjpaspring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -14,7 +16,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
