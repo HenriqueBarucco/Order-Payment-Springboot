0b844fa99808d4fbbe3efd4f72e71cede927d871
Henrique Barucco <henrique_barucco@hotmail.com>
2022-08-03 16:46:22 -0300

    User delete

--- src/main/java/com/example/workshopjpaspring/services/UserService.java
+++ src/main/java/com/example/workshopjpaspring/services/UserService.java
@@ -27,4 +27,8 @@ public class UserService {
     public User insert(User obj) {
         return userRepository.save(obj);
     }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
 }
