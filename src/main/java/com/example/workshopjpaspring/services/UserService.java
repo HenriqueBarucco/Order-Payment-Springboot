2ef46d35d1d8718de7f561fe35a0c69d7729fb02
Henrique Barucco <henrique_barucco@hotmail.com>
2022-08-03 17:28:50 -0300

    Exception handling - update

--- src/main/java/com/example/workshopjpaspring/services/UserService.java
+++ src/main/java/com/example/workshopjpaspring/services/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getOne(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getOne(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
