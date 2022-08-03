c878876c3def096fa685dc8fc6e625d9821baaae
Henrique Barucco <henrique_barucco@hotmail.com>
2022-08-02 21:30:04 -0300

    Payment, one-to-one association

--- src/main/java/com/example/workshopjpaspring/entities/Order.java
+++ src/main/java/com/example/workshopjpaspring/entities/Order.java
@@ -31,6 +31,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -77,6 +80,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
